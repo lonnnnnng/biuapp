bb7249d68af586654a8da9f0eca825a50c50b1ab
guohailong <1105744725@qq.com>
2026-08-06 23:50:29 +0800

    发布 BiuApp 0.1.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lonnnnnng.biu"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "0.1.16"
+        versionCode = 18
+        versionName = "0.1.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
